5fd10051200408b8393c5506a7f4c23217e3a371
16-Biki <mahaldarbikram@gmail.com>
2025-07-28 12:33:50 +0530

    final update

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,9 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {origin:"*"}
+));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
